1f8f43a28a8915c9ab86254b477b842df06505e3
ErikaEbon <137976326+embe8@users.noreply.github.com>
2025-08-28 22:42:54 -0700

    Create count_nodes.py

---
+++ binary tree/count_nodes.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+Returns the number of complete nodes in a binary tree
+Complete nodes = with both left and right children
+'''
+class Solution(object):
+    def countNodes(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
