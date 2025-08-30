273e99ccacf302d56ba03bb19e5ef714ac31dcef
ErikaEbon <137976326+embe8@users.noreply.github.com>
2025-08-29 20:47:10 -0700

    Create is_symmetric.py

---
+++ binary tree/is_symmetric.py
@@ -0,0 +1,30 @@
+'''
+Checks if the binary tree is symmetric
+Symmetric: left and right subtrees are mirror images of each other
+'''
+# Recursive solution
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        def helper(leftSubtree, rightSubtree):
+            if not leftSubtree and not rightSubtree:
+                return True
+
+            if not leftSubtree or not rightSubtree:
+                return False
+            if leftSubtree.val != rightSubtree.val:
+                return False
+            return helper(leftSubtree.left, rightSubtree.right) and helper(leftSubtree.right, rightSubtree.left)
+
+        if not root:
+            return True
+        return helper(root.left, root.right)
