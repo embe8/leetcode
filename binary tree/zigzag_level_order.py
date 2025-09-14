66de46899de5b4a6ab2b0bc241b93dc1e67e9d57
Erika E <137976326+embe8@users.noreply.github.com>
2025-09-14 01:24:01 -0700

    Update zigzag_level_order.py
    Added description of problem and solution

--- binary tree/zigzag_level_order.py
+++ binary tree/zigzag_level_order.py
@@ -1,9 +1,12 @@
+'''Problem: Given a binary tree, traverse it in zigzag order (root, from right to left, vice versa)'''
+'''Solution: Same as level order traversal except we need to reverse order of nodes for odd levels''
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 from collections import deque
 
 class Solution(object):
