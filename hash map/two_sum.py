b720afd7a6705baa9713804d46597fe7aa4822bd
Erika E <137976326+embe8@users.noreply.github.com>
2025-10-10 14:43:29 -0700

    Update two_sum.py
    Add brute force solution

--- hash map/two_sum.py
+++ hash map/two_sum.py
@@ -20,3 +20,18 @@ class Solution:
 
 # Brute force with O(n^2) time complexity
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        lst =[]
+        for i in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                if(nums[i]+nums[j]==target):
+                    lst.append(i)
+                    lst.append(j)
+                    return lst
+
