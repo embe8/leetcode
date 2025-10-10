c31948feb7f5b3c7c7dbcd9a00d0a294943796cf
Erika E <137976326+embe8@users.noreply.github.com>
2025-10-10 11:44:27 -0700

    Create maxProfitII.java

---
+++ array/maxProfitII.java
@@ -0,0 +1,18 @@
+// Name: Best Time to Buy And Sell Stock II
+// Java version of max_profit_ii.py
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i = 0, buy, sell, profit = 0, N = prices.length - 1;
+        while (i < N) {
+            while (i < N && prices[i + 1] <= prices[i]) {i++;}
+            buy = prices[i];
+
+            while (i < N && prices[i + 1] > prices[i]) {i++;}
+            sell = prices[i];
+
+            profit += sell - buy;
+        }
+        return profit;
+    }
+}
