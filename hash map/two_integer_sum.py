2fb176ee3edac49f4fb7b6bbd616e83899d4e5d5
Erika E <137976326+embe8@users.noreply.github.com>
2025-10-24 16:17:55 -0700

    Create two_integer_sum.py

---
+++ hash map/two_integer_sum.py
@@ -0,0 +1,18 @@
+'''Problem name: Two Integer Sum II (neetcode)
+Problem: Given a sorted array (ascending order) and a target integer, return the indices of two elements as a list such that
+both elements sum to target
+Approach: use a hash map to keep track of the  index of each element where value is the index and key is the element
+Check for target-current element in the for loop and get the index for the difference
+'''
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        maplist = {}
+        for i in range(len(numbers)):
+            if target - numbers[i] in maplist:
+                minuend = target - numbers[i]
+                index = maplist[minuend]
+                if i < index or index < i:
+                    return sorted([i+1, index+1])
+            maplist[numbers[i]] = i
+        
