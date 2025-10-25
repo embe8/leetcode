20e3058276a31b7ba0bce489b0f26bb16fb2c341
Erika E <137976326+embe8@users.noreply.github.com>
2025-10-24 19:47:42 -0700

    Update two_integer_sum.py

--- hash map/two_integer_sum.py
+++ hash map/two_integer_sum.py
@@ -1,4 +1,4 @@
-'''Problem name: Two Integer Sum II (neetcode)
+'''Problem name: Two Integer Sum II (neetcode) or Two Sum II - Input Array is Sorted (leetcode)
 Problem: Given a sorted array (ascending order) and a target integer, return the indices of two elements as a list such that
 both elements sum to target
 Approach: use a hash map to keep track of the  index of each element where value is the index and key is the element
