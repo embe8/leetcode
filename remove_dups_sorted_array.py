6d68a881c3d351197e5056f16a19962961b18b6a
Erika E <137976326+embe8@users.noreply.github.com>
2025-12-22 08:35:15 -0800

    Create remove_dups_sorted_array.py

---
+++ remove_dups_sorted_array.py
@@ -0,0 +1,17 @@
+# Problem name: Remove Duplicates from Sorted Array
+# Given a sorted array of integers return the number of unique elements
+
+class Solution(objec):
+    def removeDuplicates(self, nums):
+        if len(nums) <= 1:
+            return 1
+
+        k = 1
+
+        for i in range(1, len(nums)):
+            if(nums[1] != nums[i-1]):
+                nums[k] = nums[i]
+
+                k += 1
+        return k
+            
