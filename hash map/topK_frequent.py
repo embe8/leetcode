f244a1d1cfa0b458d79167d059b9df59014736fb
Erika E <137976326+embe8@users.noreply.github.com>
2026-01-05 21:48:24 -0800

    Update topK_frequent.py

--- hash map/topK_frequent.py
+++ hash map/topK_frequent.py
@@ -1,3 +1,5 @@
+# Given an array of integers, return the top K elements in any order
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int)->List[int]:
         mapList = {}
@@ -7,5 +9,5 @@ class Solution:
             else:
                 mapList[a] = 1
             
-        sortedList = sorted(mapList.items(), key = lambda x:x[1], reverse = True)
-        return [item[0] for item in sortedList [:k]]
+        sortedList = sorted(mapList.items(), key = lambda x:x[1], reverse = True) # take the items (ints) and sort them in descending order, use the frequency as the basis for sorting
+        return [item[0] for item in sortedList [:k]] # only take the first element (the int) of every tuple from the sortedList 
