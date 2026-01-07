2584dab6a448479541201dc6e2c2d9d7fbdf498f
Erika E <137976326+embe8@users.noreply.github.com>
2026-01-06 20:09:56 -0800

    Update group_anagrams.py

--- hash map/group_anagrams.py
+++ hash map/group_anagrams.py
@@ -36,9 +36,13 @@ class Solution:
         # sort the chars and each word, use the sorted word as key
         mapList = defaultdict(list)
         for str in strs:
-            sortedStr = ''.join(sort(str))
+            sortedStr = ''.join(sort(str)) # 'ate'->[a,e,t]-> 'aet'
             # key is sorted string and its value is the list of strings
-            mapList[sortedStr].append(s)
-        return list(mapList.values())
+            mapList[sortedStr].append(str) # aet - [ate]
+        return list(mapList.values()) # mapList.values(0) returns an object so we convert it to list
+
+# notes: mapList.values return [ate...]
+# mapList.key return aet
+# mapList.items return tuples [(aet, [ate])]
 
         
