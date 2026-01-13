217aa28d6267f953c57bc7db8e17b447d09fa526
Erika E <137976326+embe8@users.noreply.github.com>
2026-01-12 23:36:23 -0800

    Update valid_parenthesis.py

--- stack/valid_parenthesis.py
+++ stack/valid_parenthesis.py
@@ -3,6 +3,14 @@ Given a string check if it's valid where valid means that every (,[,{ should be 
 corresponding closing bracket and in the correct order (for ex. ([]) is True, [(]) is False)
 '''
 
+'''
+The input string s is valid if and only if:
+
+Every open bracket is closed by the same type of close bracket.
+Open brackets are closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+'''
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
