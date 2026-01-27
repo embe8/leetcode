c342e55197ff45325b76d8958f8471cfce627ae3
Erika E <137976326+embe8@users.noreply.github.com>
2026-01-26 20:00:56 -0800

    Update parsing_with_slicing.py

--- python_practice/parsing_with_slicing.py
+++ python_practice/parsing_with_slicing.py
@@ -5,5 +5,5 @@ sample_string = "123456789"
 cols = 4
 
 for i in range(0, len(sample_string), cols):
-    chunk = sample_string[i:i+cols]
+    chunk = sample_string[i:i+cols]  # means start:end ex. first iteration 0:0+4 so 1234 and then 2nd iteration 4:8 so 5678 
     print(''.join(chunk))
