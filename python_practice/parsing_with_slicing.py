76a36746558bb7e141ea8c44fed6454a98a4e9f4
Erika E <137976326+embe8@users.noreply.github.com>
2026-01-26 20:03:53 -0800

    Update parsing_with_slicing.py

--- python_practice/parsing_with_slicing.py
+++ python_practice/parsing_with_slicing.py
@@ -6,4 +6,4 @@ cols = 4
 
 for i in range(0, len(sample_string), cols):
     chunk = sample_string[i:i+cols]  # means start:end ex. first iteration 0:0+4 so 1234 and then 2nd iteration 4:8 so 5678 
-    print(''.join(chunk))
+    print('   '.join(chunk)) # add spaces in '' to have spacesd in between each number
