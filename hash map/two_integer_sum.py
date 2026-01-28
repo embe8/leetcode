3ec9286f335648e2a2ad6a84f532a916f737354c
Erika E <137976326+embe8@users.noreply.github.com>
2026-01-27 21:12:08 -0800

    Update two_integer_sum.py

--- hash map/two_integer_sum.py
+++ hash map/two_integer_sum.py
@@ -29,3 +29,18 @@ class Solution:
                 if index != i:
                     return sorted([index+1, i+1])
             mapList[numbers[i]] = i
+# Two pointer solution (Binary Search) time complexity: O(nlogn) space complexity: O(1)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int)->List[int]:
+        for i in range(len(numbers)):
+            left, right = i + 1, len(numbers)-1
+            diff = target - numbers[i]
+            while left <= right:
+                mid = left + (right-left)//2
+                if numbers[mid] == diff:
+                    return [i + 1, mid + 1]
+                elif numbers[mid] < diff:
+                    left = mid + 1
+                else: right = mid - 1
+        return []
+            
