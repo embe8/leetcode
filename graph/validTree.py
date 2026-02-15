6c3cb2bfeb8f20e999a08ea8b3a878ebd4d42137
Erika E <137976326+embe8@users.noreply.github.com>
2026-02-14 20:04:08 -0800

    Create validTree.py

---
+++ graph/validTree.py
@@ -0,0 +1,28 @@
+'''Neetcode: Valid Tree: Given a list of lists[int] and number of nodes n where the list are the edges, determine if the tree is valid 
+where a valid tree has no cycles and the number of edges should be n - 1
+Approach: Place the edges in an adjacency list, use a set to keep track of visisted nodes using bfs, and use a deque to keep track of nodes
+to visit, at the end if the length of visited equals to node return True'''
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]):
+        # edge case if number of edges is more than n - 1
+        if len(edges) > n - 1 : return False
+        # create the adjacency list
+        adj = [[] for _ in range(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        visited = set()
+        q = deque([(0, -1)])
+        visited.add(0)
+        while q:
+            node, parent = q.popleft()
+            for neighbor in adj[node]:
+                if neighbor == parent: continue
+
+                if neighbor in visited:
+                    return False
+
+                visited.add(neighbor)
+                q.append((neighbor, node))
+        return len(visited) == n
