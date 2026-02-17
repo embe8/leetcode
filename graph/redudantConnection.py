ea4740c864756252d64ac2efa38aa321c304b482
Erika E <137976326+embe8@users.noreply.github.com>
2026-02-17 02:06:10 -0800

    Create redudantConnection.py

---
+++ graph/redudantConnection.py
@@ -0,0 +1,39 @@
+'''Problem: Find redundant connection, given a List of integer lists named 'edges' find if there's a redundant connection
+and return the list of nodes (edge) that is redundant
+Approach: Create adjacency list and traverse tree using dfs, keep track of visited nodes and recursive dfs will return True
+if the edge has been traversed before, the function that first called the dfs will register True and return the redundant
+edge as [u, v]
+'''
+
+# Time complexity:
+# Space complexity:
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [[] for _ in range(n+1)]
+
+        def dfs(node, parent):
+            if visited[node]: return True
+
+            visited[node] = True
+            for neighbor in adj[node]:
+                if neighbor == parent:
+                    continue
+                if dfs(neighbor, node):
+                    return True
+            return False
+
+
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+            visited = [False] * (n + 1)
+
+            if dfs(u, -1):
+                return [u,v]
+        return []
+            
+        
