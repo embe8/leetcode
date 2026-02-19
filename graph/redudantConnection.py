8908f1d49c09ef272d4a875c3bedbf12a29e4ffe
Erika E <137976326+embe8@users.noreply.github.com>
2026-02-18 18:13:40 -0800

    Update redudantConnection.py

--- graph/redudantConnection.py
+++ graph/redudantConnection.py
@@ -35,5 +35,50 @@ class Solution:
             if dfs(u, -1):
                 return [u,v]
         return []
-            
+
+# Optimal solution
+# Time complexity: O(V + E)
+# Space complexity:
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]])->List[int]:
+        n = len(edges)
+        # create the adjacency list
+        adj = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        # keep track of visited nodes
+        visited = [False] * (n+1)
+        cycle = set()
+        cycleStart = -1
+
+        def dfs(node, parent):
+            nonlocal cycleStart
+            if visited[node]:
+                cycleStart = node
+                return True
+            visited[node] = True
+            for neighbor in adj[node]:
+                if neighbor == parent:
+                    continue
+                if dfs(neighbor, node):
+                    if cycleStart != -1:
+                        cycle.add(node)
+                    if node == cycleStart:
+                        cycleStart = -1
+                    return True
+            return False
+        # first call to dfs
+        dfs(1, -1) # 1 is the first node and its parent is itself
+        # find the last nodes visited in the cycle
+        for u, v in reversed(edges):
+            if u in cycle and v in cycle:
+                return [u, v]
+        return []
+
+
+
+
         
