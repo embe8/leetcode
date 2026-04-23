a6778edf35de3e63c69cb044964fb48b07fe5962
Erika E <137976326+embe8@users.noreply.github.com>
2026-04-22 23:40:30 -0700

    Update max_depth.py

--- binary tree/max_depth.py
+++ binary tree/max_depth.py
@@ -85,6 +85,24 @@ class Solution:
             level += 1
         return level
 
+# DFS version
+# time complexity: O(n)
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        s = [[root, 1]]
+
+        maxDepth = 0
+
+        while s:
+            node, depth = s.pop()
+            if node:
+                maxDepth = max(maxDepth, depth)
+                s.append([node.left, depth + 1]) # visit next
+                s.append([node.right, depth + 1])
+        return maxDepth
+
 
         
 
