6ae61f1c3b3da14bf254d99fde870167f21db529
Erika E <137976326+embe8@users.noreply.github.com>
2026-04-28 14:31:03 -0700

    Update same_tree.py

--- binary tree/same_tree.py
+++ binary tree/same_tree.py
@@ -37,5 +37,21 @@ class Solution:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         else: return False
 
+# depth first search
+# time complexity: O(n)
+# left most nodes first are checked then right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode])->bool:
+        stack = [(p, q)]
+
+        while stack:
+            node1, node2 = stack.pop()
+            if not node1 and not node2: continue
+            if not node1 or not node2 or node1.val != node2.val:
+                return False
+            stack.append((node1.right, node2.right))
+            stack.append((node1.left, node2.left)) 
+        return True
+
 
 
