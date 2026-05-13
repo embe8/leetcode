85f0d495ac010437bf5bc187ab40c1ce485fe2c4
Erika E <137976326+embe8@users.noreply.github.com>
2026-05-12 23:33:00 -0700

    Create right_side.py

---
+++ binary tree/right_side.py
@@ -0,0 +1,15 @@
+# Problem: Return as a list of integers the node values contained on the right side of a binary tree
+# Approach: use DFS and check when depth equals length of current list (should be 1), append to result list if so
+
+class Solution:
+  def rightSide(self, root: Optional [TreeNode])->List[int]:
+    res = [] # initialize list to be returned
+    def dfs(node, depth):
+      if not node: return None
+      if depth == len(res) # then it's the first node
+        res.append(node.val)
+      dfs(node.right, depth + 1)
+      dfs(node.left, depth + 1)
+
+    dfs(root, 0)
+    return res
