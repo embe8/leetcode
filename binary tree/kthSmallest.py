5cda0834162db8d776cff215e0481c6117d262a5
Erika E <137976326+embe8@users.noreply.github.com>
2026-07-09 12:58:06 -0700

    Update kthSmallest.py

--- binary tree/kthSmallest.py
+++ binary tree/kthSmallest.py
@@ -1,1 +1,9 @@
 
+
+class Solution:
+  def kthSmallest(self, root: Optional[TreeNode])-> bool:
+    if not root: return True
+    q.deque([(root, float("-inf"), float("inf")])
+    while q:
+      node, left, right = q.popleft()
+      if not ()
