49cfe6afc7217a657380a5dcbfdf998b13ca7524
Erika E <137976326+embe8@users.noreply.github.com>
2026-07-31 17:01:39 -0700

    Update longestSubstring2.py

--- sliding_window/longestSubstring2.py
+++ sliding_window/longestSubstring2.py
@@ -1,1 +1,18 @@
+# longest repeating character replacement in neetcode: find the longest substring with repeating character of k (that can be replaced) return its length
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        res = 0
+        charSet = set(s)
 
+        for c in charSet:
+            count = l = 0
+            for r in range(len(s)):
+                if s[r] == c:
+                    count += 1
+                while (r-l+1)-count > k: # length of current window - count is more than k (invalid)
+                    if s[l] == c:
+                        count -= 1
+                    l += 1
+                res = max(res, r-l+1)
+        return res
+        
