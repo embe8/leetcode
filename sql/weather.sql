82cd86a13cf613d634c61be3f6a9040bdfe68f1c
Erika E <137976326+embe8@users.noreply.github.com>
2026-09-04 10:13:30 -0700

    Update weather.sql

--- sql/weather.sql
+++ sql/weather.sql
@@ -6,3 +6,8 @@ FROM Weather yesterday
 # difference between date 1 and date 2 should be 1
 # and temperature in 
 CROSS JOIN Weather today# join with itself
+
+#alternate solution joins table with itself
+SELECT w1.id
+FROM Weather w1, Weather w2
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
