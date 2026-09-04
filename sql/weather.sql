412c34a54ad5e9c3e0b0102ccc399cc1e5af8b87
Erika E <137976326+embe8@users.noreply.github.com>
2026-09-04 10:33:19 -0700

    Update weather.sql

--- sql/weather.sql
+++ sql/weather.sql
@@ -11,3 +11,9 @@ CROSS JOIN Weather today# join with itself
 SELECT w1.id
 FROM Weather w1, Weather w2
 WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+
+#
+#Join the Weather table with itself, denoting the first occurrence as w1 and the second occurrence as w2.
+#Compare the dates of w1 and w2 using the DATEDIFF() function to check if they are consecutive days (with a difference of 1 day).
+#Add a condition in the WHERE clause to select the rows where the temperature of w1 is greater than the temperature of w2.
+#Select the id of w1 as the result.
