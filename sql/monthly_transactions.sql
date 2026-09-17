7443651d7e008f516e56408eb893d5bd91797bbf
Erika E <137976326+embe8@users.noreply.github.com>
2026-09-16 19:56:02 -0700

    Update monthly_transactions.sql

--- sql/monthly_transactions.sql
+++ sql/monthly_transactions.sql
@@ -36,3 +36,16 @@ FROM
 GROUP BY 
     DATE_FORMAT(trans_date, '%Y-%m'),
     country
+
+'''version 2'''
+# Write your MySQL query statement below
+SELECT
+    LEFT(trans_date, 7) AS month, 
+    country,
+    COUNT(id) AS trans_count, 
+    SUM(state = 'approved') AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM((state = 'approved') * amount) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country
+
